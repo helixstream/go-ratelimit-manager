ca8373ff1e93d04b72da02836179beed02faba58
David Klingler <deweydbb@gmail.com>
2019-06-24 10:07:05 -0500

    testing

--- src/host/server.go
+++ src/host/server.go
@@ -1,9 +1,10 @@
 package host
 
 import (
-	"github.com/youtube/vitess/go/ratelimiter"
+	"fmt"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -12,9 +13,9 @@ var (
 	sustainedDuration = time.Minute
 	burstDuration     = time.Second
 
-	sustainedLimiter = ratelimiter.NewRateLimiter(serverConfig.SustainedRequestLimit, sustainedDuration)
-	burstLimiter     = ratelimiter.NewRateLimiter(serverConfig.BurstRequestLimit, burstDuration)
-	bannedLimiter    = ratelimiter.NewRateLimiter(10, sustainedDuration)
+	sustainedLimiter = NewRateLimiter(serverConfig.SustainedRequestLimit, sustainedDuration)
+	burstLimiter     = NewRateLimiter(serverConfig.BurstRequestLimit, burstDuration)
+	bannedLimiter    = NewRateLimiter(10, sustainedDuration)
 
 	port = "8000"
 )
@@ -27,6 +28,7 @@ func serveHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if sustainedLimiter.Allow() && burstLimiter.Allow() {
+		fmt.Print(burstLimiter.curCount)
 		w.WriteHeader(200)
 	} else if bannedLimiter.Allow() {
 		http.Error(w, "Too many requests", 429)
@@ -49,4 +51,42 @@ func getServer() *http.Server {
 	}()
 
 	return server
+}
+
+type RateLimiter struct {
+	maxCount int
+	interval time.Duration
+
+	mu       sync.Mutex
+	curCount int
+	lastTime time.Time
+}
+
+// NewRateLimiter creates a new RateLimiter. maxCount is the max burst allowed
+// while interval specifies the duration for a burst. The effective rate limit is
+// equal to maxCount/interval. For example, if you want to a max QPS of 5000,
+// and want to limit bursts to no more than 500, you'd specify a maxCount of 500
+// and an interval of 100*time.Millilsecond.
+func NewRateLimiter(maxCount int, interval time.Duration) *RateLimiter {
+	return &RateLimiter{
+		maxCount: maxCount,
+		interval: interval,
+	}
+}
+
+// Allow returns true if a request is within the rate limit norms.
+// Otherwise, it returns false.
+func (rl *RateLimiter) Allow() bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+	if time.Since(rl.lastTime) < rl.interval {
+		if rl.curCount > 0 {
+			rl.curCount--
+			return true
+		}
+		return false
+	}
+	rl.curCount = rl.maxCount - 1
+	rl.lastTime = time.Now()
+	return true
 }
